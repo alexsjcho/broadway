abc4b7bbfee720adc2a6a2cc43fc051246664745
Alex Cho <alexsjcho@gmail.com>
2018-10-22 16:13:11 -0700

    render images on index page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_22_032637) do
+ActiveRecord::Schema.define(version: 2018_10_22_224446) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,10 @@ ActiveRecord::Schema.define(version: 2018_10_22_032637) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.integer "category_id"
+    t.string "play_img_file_name"
+    t.string "play_img_content_type"
+    t.bigint "play_img_file_size"
+    t.datetime "play_img_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
